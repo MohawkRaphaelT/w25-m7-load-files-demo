8c188414c0b5e601ff33e8ac8fcc68db1fcc99c0
MohawkRaphaelT <150467892+MohawkRaphaelT@users.noreply.github.com>
2025-03-11 09:42:01 -0400

    Add image and loading example

--- load-asset-demo/Game.cs
+++ load-asset-demo/Game.cs
@@ -1,5 +1,6 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
+using System.IO;
 using System.Numerics;
 
 // The namespace your code is in.
@@ -10,15 +11,21 @@ namespace MohawkGame2D
     /// </summary>
     public class Game
     {
-        // Place your variables here:
-
+        const string assetFolder = "../../../../assets/";
+        // 
+        Texture2D texture =
+            Graphics.LoadTexture(assetFolder + "graphics/raph 2d anim idle.png");
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            Window.SetTitle("Load asset demo");
+            Window.SetSize(400, 400);
 
+            string cwd = Directory.GetCurrentDirectory();
+            Console.WriteLine($"DIRECTORY: {cwd}");
         }
 
         /// <summary>
@@ -26,7 +33,9 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.OffWhite);
 
+            Graphics.Draw(texture, 10, 10);
         }
     }
 
